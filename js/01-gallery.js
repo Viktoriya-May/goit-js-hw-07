46df6e4b55451702d7756eeb4fd00264c4141570
Viktoriya May <119529675+Viktoriya-May@users.noreply.github.com>
2023-04-28 00:16:51 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const paletteContainer = document.querySelector('.gallery');
+
+const galleryEl = galleryItems
+    .map(({ preview, description, original }) => 
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`)
+    .join('');
+
+    paletteContainer.insertAdjacentHTML('beforeend', galleryEl)
+
+    paletteContainer.addEventListener('click', onPaletteContainerClick)
+
+function onPaletteContainerClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const modal = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}" width="1280" height="600">`,
+
+        {   onShow: () => window.addEventListener('keydown', onEscKeyPress),
+            onClose: () => window.removeEventListener('keydown', onEscKeyPress),
+        }
+    );
+    
+    modal.show();
+
+    function onEscKeyPress(evt) {   
+        if (evt.code === "Escape") {
+            modal.close();
+        }
+    }
+}
