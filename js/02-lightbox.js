3b6aa1db84d197da995c08a6ab2e3b9e8b17bd9c
Viktoriya May <119529675+Viktoriya-May@users.noreply.github.com>
2023-04-28 00:37:24 +0200

    add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const paletteContainer = document.querySelector('.gallery');
+
+const galleryEl = galleryItems
+    .map(({ preview, description, original }) => 
+    `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`)
+    .join('');
+
+    paletteContainer.insertAdjacentHTML('beforeend', galleryEl)
+
+    new SimpleLightbox(".gallery a", {
+        overlayOpacity: 0.5,
+        captionDelay: 250,
+        captionsData: "alt",
+        fadeSpeed: 250,
+        rtl: true,
+      });
